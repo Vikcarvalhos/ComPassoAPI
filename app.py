731b8ee8e0559d562a905cdd0ed641090850070a
João Viktor <viktorcarvalhos@gmail.com>
2024-10-23 17:28:57 -0300

    removing init

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from app import create_app
+from flask import Flask
+from flask_cors import CORS
+from app.routes import main  # Importar diretamente as rotas
 
+def create_app():
+    app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    # Registrar as rotas
+    app.register_blueprint(main)
+
+    return app
+
+# Criar a aplicação
 app = create_app()
 
+# Executar a aplicação se o arquivo for chamado diretamente
 if __name__ == "__main__":
     app.run(debug=False)
